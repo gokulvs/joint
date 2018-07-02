e5b95e96d759b452d23bc719d30b3cd25fb16bea
Rohith <r.rohith@6dtech.co.in>
2018-07-02 17:31:28 +0530

    initial code

---
+++ index.js
@@ -0,0 +1,24 @@
+var express=require("express");
+var app=express();
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+app.use(express.static('public'))
+
+app.get('/', function(req, res, next) { res.send('Hello world!'); });
+
+var server =  app.listen(8000,()=>{
+    console.log("APP STARTED ON 8000");
+});
+
+var options = {
+    debug: true
+}
+var peerserver = ExpressPeerServer(server, options);
+peerserver.on('connection', function(id) { 
+    console.log("client connected");
+});
+peerserver.on('disconnect', function(id) { 
+    console.log("client disconnected");
+});
+
+app.use('/peerjs', peerserver);
+
