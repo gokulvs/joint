18074d39fbf3ccb39b68c8f8f7568ea4c52e7cc3
gokulvs <gokulvsmail@gmail.com>
2018-07-02 17:42:21 +0530

    files with peer js

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+var ExpressPeerServer = require('peer').ExpressPeerServer;
+
+app.use(express.static('public'))
+
+app.get('/', function(req, res, next) { res.send('Hello world!'); });
+
+var server = app.listen(9000,function(err,data){
+    console.log("server up and running");
+});
+
+var options = {
+    debug: true
+}
+
+var peerserver = ExpressPeerServer(server, options);
+
+app.use('/api', peerserver);
+
+
